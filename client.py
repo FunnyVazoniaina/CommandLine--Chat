95ec1af3bd1392202c712be1e2a96155929b9fd9
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-06-30 09:45:10 +0200

    Feat[server/client]: Implementing functionalities like registration, user list and password cryto

--- client.py
+++ client.py
@@ -7,27 +7,34 @@ def receive(sock):
             msg = sock.recv(1024).decode()
             print(msg)
         except:
-            print("Déconnecté du serveur.")
+            print("Déconnecté du serveur")
             sock.close()
             break
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("127.0.0.1", 5000))
 
-pseudo = input("Votre pseudo : ")
-password = input("Mot de passe : ")
+# Interface de connexion / inscription
+choice = input("Tapez 1 pour se connecter, 2 pour s'inscrire: ")
+pseudo = input("Votre pseudo: ")
+password = input("Mot de passe: ")
 
-print(client.recv(1024).decode())  # "Pseudo: "
+print(client.recv(1024).decode())  # demande choix
+client.send(choice.encode())
+print(client.recv(1024).decode())  # demande pseudo
 client.send(pseudo.encode())
-print(client.recv(1024).decode())  # "Mot de passe: "
+print(client.recv(1024).decode())  # demande mot de passe
 client.send(password.encode())
 
 response = client.recv(1024).decode()
-if response != "AUTH_OK":
+if response == "PSEUDO_EXIST":
+    print("Ce pseudo existe déjà.")
+    client.close()
+elif response != "AUTH_OK":
     print("Authentification échouée.")
     client.close()
 else:
-    print("Connecté ! Tapez vos messages.")
+    print("Connecté au chat ! Tapez vos messages (/msg, /list, /exit)")
     threading.Thread(target=receive, args=(client,), daemon=True).start()
     while True:
         msg = input()
--- serveur.py
+++ serveur.py
@@ -1,23 +1,52 @@
 import socket
 import threading
 import json
+import hashlib
+import os
 
 clients = {}  # socket: pseudo
 pseudos = set()
+users_file = "users.json"
 
-with open("users.json", "r") as f:
+# Charger ou créer le fichier d'utilisateurs
+if not os.path.exists(users_file):
+    with open(users_file, "w") as f:
+        json.dump({}, f)
+
+with open(users_file, "r") as f:
     users = json.load(f)
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def save_users():
+    with open(users_file, "w") as f:
+        json.dump(users, f)
 
 def handle_client(client_socket):
     try:
+        client_socket.send("Tapez 1 pour connexion, 2 pour inscription: ".encode())
+        choice = client_socket.recv(1024).decode()
+
         client_socket.send("Pseudo: ".encode())
         pseudo = client_socket.recv(1024).decode()
-
         client_socket.send("Mot de passe: ".encode())
         password = client_socket.recv(1024).decode()
 
-        if pseudo not in users or users[pseudo] != password or pseudo in pseudos:
-            client_socket.send("AUTH_FAIL".encode())
+        if choice == "1":  # Connexion
+            if pseudo not in users or users[pseudo] != hash_password(password) or pseudo in pseudos:
+                client_socket.send("AUTH_FAIL".encode())
+                client_socket.close()
+                return
+        elif choice == "2":  # Inscription
+            if pseudo in users:
+                client_socket.send("PSEUDO_EXIST".encode())
+                client_socket.close()
+                return
+            users[pseudo] = hash_password(password)
+            save_users()
+        else:
+            client_socket.send("CHOIX_INVALIDE".encode())
             client_socket.close()
             return
 
@@ -28,7 +57,10 @@ def handle_client(client_socket):
 
         while True:
             msg = client_socket.recv(1024).decode()
-            if msg.startswith("/msg"):
+            if msg == "/list":
+                connected = ", ".join(pseudos)
+                client_socket.send(f"Connectés: {connected}".encode())
+            elif msg.startswith("/msg"):
                 parts = msg.split(" ", 2)
                 if len(parts) == 3:
                     dest_pseudo, message = parts[1], parts[2]
@@ -63,8 +95,8 @@ def send_private(from_user, to_user, message):
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("0.0.0.0", 5000))
 server.listen()
-print("Serveur lancé sur le port 5000")
+print("Serveur en cours sur le port 5000")
 
 while True:
     client, _ = server.accept()
-    threading.Thread(target=handle_client, args=(client,)).start()
+    threading.Thread(target=handle_client, args=(client,), daemon=True).start()
